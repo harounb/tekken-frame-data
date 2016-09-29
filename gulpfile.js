5d95016d2da38066b8df8f28c26e5024c6a5895c
Haroun Butt <dekuharoun@gmail.com>
2016-09-29 14:11:51 +0100

    added gulp tasks for CI build and local formatting

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var jsonlint = require("gulp-jsonlint");
+var gulp = require("gulp");
+var diff = require('gulp-diff');
+var beautify = require('gulp-beautify');
+var src = [
+  "./package.json",
+  "./.jsbeautifyrc",
+  "./ttt2/**/*.json",
+  "./t7/**/*.json"
+];
+
+gulp.task("formatcheck", function () {
+  return gulp.src(src)
+    .pipe(beautify())
+    .pipe(diff())
+    // emit an error on finding diffs
+    .pipe(diff.reporter({ fail: true }));
+
+});
+
+gulp.task("jsonlint", function () {
+  return gulp.src(src)
+    .pipe(jsonlint())
+    .pipe(jsonlint.failOnError())
+    .pipe(jsonlint.reporter())
+	.pipe(diff.reporter({ fail: true }));
+})
+
+gulp.task("format", function () {
+  return gulp.src(src, {base: "./"})
+    .pipe(beautify())
+	.pipe(gulp.dest('.'));
+})
+
+gulp.task("ci", ["jsonlint", "formatcheck"])
