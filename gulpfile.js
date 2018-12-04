839b22d9133d0a3fc3e35294162d7fa58f40d4f4
Haroun <dekuharoun@gmail.com>
2018-12-04 15:08:19 +0000

    moved to prettier from jsbeautify

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,13 @@
 var jsonlint = require("gulp-jsonlint");
 var gulp = require("gulp");
-var diff = require("gulp-diff");
-var beautify = require("gulp-beautify");
-var src = ["./.jsbeautifyrc", "./ttt2/**/*.json", "./t7/**/*.json"];
-
-gulp.task("formatcheck", function() {
-  return (
-    gulp
-      .src(src)
-      .pipe(beautify())
-      .pipe(diff())
-      // emit an error on finding diffs
-      .pipe(diff.reporter({ fail: true }))
-  );
-});
+var src = ["./ttt2/**/*.json", "./t7/**/*.json"];
 
 gulp.task("jsonlint", function() {
   return gulp
     .src(src)
     .pipe(jsonlint())
     .pipe(jsonlint.failOnError())
-    .pipe(jsonlint.reporter())
-    .pipe(diff.reporter({ fail: true }));
+    .pipe(jsonlint.reporter());
 });
 
-gulp.task("format", function() {
-  return gulp
-    .src(src, { base: "./" })
-    .pipe(beautify())
-    .pipe(gulp.dest("."));
-});
-
-gulp.task("ci", ["jsonlint", "formatcheck"]);
+gulp.task("ci", ["jsonlint"]);
